0603e61e7779b658f62776cdb16227f494b9752d
Vamsi Krishna <uppalapuvamsikrishna@gmail.com>
2025-05-14 16:47:33 +0530

    index.js

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function loop(a){
     }
     result.push([a[a.length-1],a[0]])
     return result;         
-}
+    }
 let adjacentpairs = [
     [1,7],[2,8],[5,11],[6,12],[9,15],[10,16]
-];
+    ];
 adjacentpairs = adjacentpairs.concat(loop([0,1,3,5,4,2]));
 adjacentpairs = adjacentpairs.concat(loop([6,7,9,11,10,8]));
 adjacentpairs = adjacentpairs.concat(loop([12,13,15,17,16,14]));
@@ -35,7 +35,7 @@ function AddAdj(arr){
     let [a,b] = arr;
     adjacency[a][b]=1;
     adjacency[b][a]=1;
-}
+     }
 document.getElementById("rscore").textContent = 0;
 document.getElementById("bscore").textContent = 0;
 document.getElementById("player").textContent = turn.charAt(0).toUpperCase() + turn.slice(1);
@@ -48,7 +48,7 @@ function place(num){
         turn = "blue";
         occupied[num] = true;
         reds-=1;
-    }
+      }
     else if (turn==="blue"&&!occupied[num]&&blues!==0){
         document.getElementById(`circle${String(num)}`).style.backgroundColor = "blue";
         toggle();
@@ -56,134 +56,131 @@ function place(num){
         occupied[num] = true;
         blues-=1;
     }
-}
+    }
 
 function activate(num){
     active = num;
     console.log("active",active);
     document.getElementById(`circle${String(num)}`).style.opacity = 0.7;
-}
+    }
 function deactivate(num){
     active = null;
     document.getElementById(`circle${String(num)}`).style.opacity = 1;
-}
+    }
 
 function getLayer(num) {
     if (num < 6) return "outer";
     if (num < 12) return "middle";
     return "inner";
-}
+    }
 
 
 function placementstage(callback,num){
-    if (!paused){
-        if (!middleunlocked){
-            if(num<6){
-                place(num);
-                outer += 1;
-                document.getElementById("player").textContent = turn.charAt(0).toUpperCase() + turn.slice(1);
-                document.getElementById("player").style.color = turn;
-                if (outer===6){middleunlocked=true;}
-            }
-        }
-        else if (middleunlocked ){
-            if (reds!==0 || blues!==0){
-                if(num<12){
-                    place(num);
-                    middle += 1;
-                    document.getElementById("player").textContent = turn.charAt(0).toUpperCase() + turn.slice(1);
-                    document.getElementById("player").style.color = turn;
-                }
-            }
-            else if (reds===0 && blues ===0){
-                callback(move,num);
-            }
-        }
-        setTimeout(()=>{
-            if (inner===6){
-                finish();
-                if(rsc>bsc){
-                    window.alert("Red wins");
-                }
-                else if (bsc>rsc){
-                    window.alert("Blue wins");
-                }
-                else{
-                    window.alert("Draw");
-                }
-                reset()
-            }
-        });
-    }
-
+if (!paused){
+if (!middleunlocked){
+if(num<6){
+place(num);
+outer += 1;
+document.getElementById("player").textContent = turn.charAt(0).toUpperCase() + turn.slice(1);
+document.getElementById("player").style.color = turn;
+if (outer===6){middleunlocked=true;}
+}
+}
+else if (middleunlocked ){
+if (reds!==0 || blues!==0){
+if(num<12){
+place(num);
+middle += 1;
+document.getElementById("player").textContent = turn.charAt(0).toUpperCase() + turn.slice(1);
+document.getElementById("player").style.color = turn;
+}
+}
+else if (reds===0 && blues ===0){
+callback(move,num);
+}
+}
+setTimeout(()=>{
+if (inner===6){
+finish();
+if(rsc>bsc){
+window.alert("Red wins");
+}
+else if (bsc>rsc){
+window.alert("Blue wins");
+}
+else{
+window.alert("Draw");
+}
+reset()
+}
+});
+}
     console.log(outer);
     console.log(middle);
     console.log(inner);
-}
+ }
 
 function movementstage(callback,num){
-    if (!paused){
-        if (reds!==0||blues!==0){
-            place(num);
-            middle += 1;
-        }
-        if (reds===0 && blues ===0){
-            if(active===null && occupied[num]){
-                let color = document.getElementById(`circle${num}`).style.backgroundColor;
-                if ((turn === "red" && color === "red") || (turn === "blue" && color === "blue")) {activate(num);}
-                }
-                else if (active!== null && num===active){
-                    deactivate(num);
-                }
-                else if (active!==null && !occupied[num]){
-                    callback(active,num);
-                }
-        }
-    }
-    document.getElementById("player").textContent = turn.charAt(0).toUpperCase() + turn.slice(1);
-    document.getElementById("player").style.color = turn;
+if (!paused){
+if (reds!==0||blues!==0){
+place(num);
+middle += 1;  
+}
+if (reds===0 && blues ===0){
+if(active===null && occupied[num]){
+let color = document.getElementById(`circle${num}`).style.backgroundColor;
+if ((turn === "red" && color === "red") || (turn === "blue" && color === "blue")) {activate(num);}
+ }
+ else if (active!== null && num===active){
+ deactivate(num);
+}
+else if (active!==null && !occupied[num]){
+callback(active,num);
+}
+}
+}
+document.getElementById("player").textContent = turn.charAt(0).toUpperCase() + turn.slice(1);
+document.getElementById("player").style.color = turn;
 }
 
 function move(num1,num2){
 
-    if (adjacency[num1][num2] === 1 && num1 === active && !occupied[num2]){
-        let fromLayer = getLayer(num1);
-        let toLayer = getLayer(num2);  
-        if (toLayer!=="inner"||(toLayer==="inner"&&innerunlocked)){
-    if (fromLayer === "outer") outer -= 1;
-    if (fromLayer === "middle") middle -= 1;
-    if (fromLayer === "inner") inner -= 1;
+if (adjacency[num1][num2] === 1 && num1 === active && !occupied[num2]){
+let fromLayer = getLayer(num1);
+let toLayer = getLayer(num2);  
+if (toLayer!=="inner"||(toLayer==="inner"&&innerunlocked)){
+if (fromLayer === "outer") outer -= 1;
+if (fromLayer === "middle") middle -= 1;
+if (fromLayer === "inner") inner -= 1;
 
-    if (toLayer === "outer") outer += 1;
-    if (toLayer === "middle") middle += 1;
-    if (toLayer === "inner") inner += 1;
+if (toLayer === "outer") outer += 1;
+if (toLayer === "middle") middle += 1;
+if (toLayer === "inner") inner += 1;
     
-    if (middle === 6){innerunlocked = true;}
-        }
-    }
-
-
-
-    if (!paused){
-        if ( ((!innerunlocked) && (num2<12))|| innerunlocked){
-            if (turn === "blue" && adjacency[num1][num2]===1){
-                document.getElementById(`circle${String(num1)}`).style.backgroundColor = "";
-                document.getElementById(`circle${String(num2)}`).style.backgroundColor = "blue";
-                toggle();
-                turn = "red";
-                occupied[num1] = false;
-                occupied[num2] = true;
-            }
-            else if (turn === "red" && adjacency[num1][num2] ===1){
-                document.getElementById(`circle${String(num1)}`).style.backgroundColor = "";
-                document.getElementById(`circle${String(num2)}`).style.backgroundColor = "red";
-                toggle();
-                turn = "blue";
-                occupied[num1] = false;
-                occupied[num2] = true;
-            }
-            deactivate(num1);
-    }
+if (middle === 6){innerunlocked = true;}
+}
+}
+    
+if (!paused){
+if ( ((!innerunlocked) && (num2<12))|| innerunlocked){
+if (turn === "blue" && adjacency[num1][num2]===1){
+document.getElementById(`circle${String(num1)}`).style.backgroundColor = "";
+document.getElementById(`circle${String(num2)}`).style.backgroundColor = "blue";
+toggle();
+turn = "red";
+occupied[num1] = false;
+occupied[num2] = true;
+}
+else if (turn === "red" && adjacency[num1][num2] ===1){
+document.getElementById(`circle${String(num1)}`).style.backgroundColor = "";
+document.getElementById(`circle${String(num2)}`).style.backgroundColor = "red";
+toggle();
+turn = "blue";
+occupied[num1] = false;
+occupied[num2] = true;
+}
+deactivate(num1);
+}
 }
 }
 
@@ -215,4 +212,4 @@ function finish(){
     );
     document.getElementById("rscore").textContent = `${rsc}`;
     document.getElementById("bscore").textContent = `${bsc}`;
-}
+    }
